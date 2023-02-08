7f49dc6714ac70bd151130b2487c72cd72882b5c
AlexVic1 <105377287+AlexVic1@users.noreply.github.com>
2023-02-08 14:14:33 +0200

    add about page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,9 +9,13 @@ def home():
 
 @app.route("/api/v1/<station>/<date> ")
 def about(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = df.read_csv(filename, skiprows=20, parse_datets=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
